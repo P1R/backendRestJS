6b088eba3c1a6a2d1a703137a8e754e400b797af
David E. Perez Negron R <david@neetsec.com>
2023-03-11 19:32:56 +0000

    Adding read Only Data

---
+++ src/app.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json'
+//import * as dotenv from 'dotenv';
+//dotenv.config();
+
+
+//contract Address and Network
+const CONTRACT_ADDRESS = 
+  "0x18dF1C9a5c9A7A35c251818Eec22ccaf3905fe3D"
+const NETWORK = "maticmum"
+
+@Injectable()
+export class AppService {
+  provider: ethers.providers.Provider;
+  contract: ethers.Contract;
+
+  constructor(){
+    this.provider = new ethers.providers.InfuraProvider(
+      NETWORK,
+      process.env.INFURA_API_KEY
+    );
+    this.contract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      tokenJson.abi,
+      this.provider
+    )
+  }
+
+  getContractAddress(): string {
+    return this.contract.address;
+  }
+
+  async getTotalSupply(): Promise<number> {
+    const totalSupplyBN = await this.contract.totalSupply();
+    const totalSupplyString = ethers.utils.formatEther(totalSupplyBN);
+    // better way for small numbers supply
+    const totalSupplyNumber = Math.round(parseFloat(totalSupplyString) * (10 ** 18))
+    //const totalSupplyNumber = parseFloat(totalSupplyString);
+    return totalSupplyNumber;
+  }
+  
+  async getAllowance(from: string, to: string):
+  Promise<number> {
+    const allowanceBN = await this.contract.allowance(
+      from,
+      to
+    );
+    const allowanceString = ethers.utils.formatEther(allowanceBN);
+    //const allowanceNumber = Math.round(parseFloat(allowanceString) * (10 ** 18));
+    const allowanceNumber = parseFloat(allowanceString);
+    return allowanceNumber;
+  }
+
+  async getTransactionStatus(hash: string): Promise<string>{
+    const tx = await this.provider.getTransaction(hash);
+    const txRecipt = await tx.wait();
+    return txRecipt.status == 1 ? "Completed" : "Reverted";
+  }
+
+}
